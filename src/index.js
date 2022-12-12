1bad7ba03f28b4395d97a88e7fc283139125b1a2
Обоенкова Владилена <vladaoboenkova@gmail.com>
2022-12-13 00:04:55 +0300

    Добавлена функция для расчета ИМТ

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
-
 const app = express();
 
 app.use(express.static(__dirname + "/page-files"));
 
 const parser = express.urlencoded({extended: false});
+
+function countBMI(weight, height){
+    const heightInMetres = height/100;
+    return (weight / (heightInMetres * heightInMetres)).toFixed(2);
+}
 
 app.get("/", function (request, response) {
     response.sendFile(__dirname + "/page-files/index.html");
@@ -12,7 +16,7 @@ app.get("/", function (request, response) {
 
 app.post("/", parser, function(request, response) {
     if (!request.body) return response.sendStatus(400);
-    console.log(`Вес: ${request.body.weight}, Рост: ${request.body.height}`);
+    response.send(`${countBMI(request.body.weight, request.body.height)}`);
 });
 
 const server = app.listen(3000, ()=>console.log("Сервер запущен..."));
